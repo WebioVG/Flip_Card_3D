f920afbcc418bb4d069699cb7589167560e3ae5e
webiovg <kevin.kowalsky1@gmail.com>
2022-05-22 22:03:19 +0200

    flip card fonnctionnelle

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ let cards_back = document.getElementsByClassName('card_back');
 // Main
 for (let card in cards) {    
     cards[card].addEventListener('mouseenter', function() {
+        cards_back[card].style.transitionDelay = '250ms';
+        cards_front[card].style.transitionDelay = '0ms';
+
         cards_front[card].classList.add('card_front_active');
         cards_back[card].classList.add('card_back_active');
     })
     
     cards[card].addEventListener('mouseleave', function() {
+        cards_back[card].style.transitionDelay = '0ms';
+        cards_front[card].style.transitionDelay = '250ms';
+
         cards_front[card].classList.remove('card_front_active');
         cards_back[card].classList.remove('card_back_active');
     })
